3ff793c4dc19b6082941588e108f0e497116fec3
Donavan Watley <76824069+donnyboy72@users.noreply.github.com>
2025-11-10 10:37:13 -0500

    Add files via upload

--- 3DRender/src/Demo3D.java
+++ 3DRender/src/Demo3D.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Path2D;
+import java.awt.image.BufferedImage;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -70,29 +71,76 @@ public class Demo3D {
 				Matrix3 transform = horizTransform.multiply(vertTransform);
 				
 				//add triangles to screen
-				g2.translate(getWidth() / 2, getHeight() / 2);
-				g2.setColor(Color.WHITE);
+				BufferedImage img = 
+					    new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
+				
+				// initialize z-buffer (same size as the image)
+				double[] zBuffer = new double[img.getWidth() * img.getHeight()];
+				for (int q = 0; q < zBuffer.length; q++) {
+				    zBuffer[q] = Double.NEGATIVE_INFINITY;
+				}
+				
 				for(Triangle t : tris) 
 				{	
 					Vertex v1 = transform.transform(t.v1);
 					Vertex v2 = transform.transform(t.v2);
 					Vertex v3 = transform.transform(t.v3);
-					Path2D path = new Path2D.Double();
-					path.moveTo(v1.x, v1.y);
-					path.lineTo(v2.x, v2.y);
-					path.lineTo(v3.x, v3.y);
-					path.closePath();
-					g2.draw(path);
+					v1.x += getWidth() / 2;
+				    v1.y += getHeight() / 2;
+				    v2.x += getWidth() / 2;
+				    v2.y += getHeight() / 2;
+				    v3.x += getWidth() / 2;
+				    v3.y += getHeight() / 2;
+				
+				    
+				 // compute rectangular bounds for triangle
+				    int minX = (int) Math.max(0, Math.ceil(Math.min(v1.x, Math.min(v2.x, v3.x))));
+				    int maxX = (int) Math.min(img.getWidth() - 1, 
+				                              Math.floor(Math.max(v1.x, Math.max(v2.x, v3.x))));
+				    int minY = (int) Math.max(0, Math.ceil(Math.min(v1.y, Math.min(v2.y, v3.y))));
+				    int maxY = (int) Math.min(img.getHeight() - 1,
+				                              Math.floor(Math.max(v1.y, Math.max(v2.y, v3.y))));
+				    
+				    double triangleArea =
+				    	       (v1.y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - v1.x);
+
+				    	    for (int y = minY; y <= maxY; y++) {
+				    	        for (int x = minX; x <= maxX; x++) {
+				    	            double b1 = 
+				    	              ((y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - x)) / triangleArea;
+				    	            double b2 =
+				    	              ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x - x)) / triangleArea;
+				    	            double b3 =
+				    	              ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / triangleArea;
+				    	            if (b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1) {
+				    	                // interpolate depth using barycentric coordinates
+				    	                double depth = b1 * v1.z + b2 * v2.z + b3 * v3.z;
+
+				    	                // index into the zBuffer
+				    	                int zIndex = y * img.getWidth() + x;
+
+				    	                // draw only if this pixel is closer
+				    	                if (zBuffer[zIndex] < depth) {
+				    	                    img.setRGB(x, y, t.color.getRGB());
+				    	                    zBuffer[zIndex] = depth;
+				    	                }
+				    	            }
+				    	        }
+				    	        
+				    	    }
+				    	    
+				    g2.drawImage(img,0,0,null);
 				}
 			}
 		};
+		
 		//add listeners for horiznotal slider and vertical slider
 		hS.addChangeListener(e-> renderPanel.repaint());
 		vS.addChangeListener(e-> renderPanel.repaint());
 		
 		pane.add(renderPanel, BorderLayout.CENTER);//add the renderPanel to the main JFrame
 		
-		f.setSize(600,600);//set size of JFrame
+		f.setSize(1200,600);//set size of JFrame
 		f.setVisible(true);
 	}
 	
